8cdfecf7bbac64417526bc14da30e090cec7ee6a
dima_samoylov_84@mail.ru <student@FB.LOCAL>
2025-09-24 18:54:37 +0700

    Картинка

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/web")
 def start ():
-    return "web-сервер на flask"
+    return "web-сервер на flask" \
+             "<html>"  \
+             "     <body>"  \
+             "          <h1>web-сервер на flask</h1>"  \
+             "      </body>"  \
+             "</html>" 
+
+@app.route("/author")
+def author():
+    name = "Самойлов Дмитрий Алексеевич"
+    group = "ФБИ-32"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+            <html>
+                <body>
+                    <p>Студент: """ + name + """ </p>
+                    <p>Группа: """ + group + """ </p>
+                    <p>Факультет: """ + faculty + """</p>
+                    <a href="/web">web</a>
+                </body>
+            </html>"""
+                
+@app.route('/image')
+def image():
+    path = url_for("static", filename="oak.jpg")
+    return '''
+<!doctype html>
+ <html>
+        <body>
+            <h1>Дуб</h1>
+            <img src="'''+ path + '''">
+        </body>
+</html>
+'''
