0f693fb9e99f490e0fca46830b8c611f9aaf5720
Самойлов Дмитрий <dima_samoylov_84@mail.ru>
2025-11-05 15:27:26 +0300

    Стили

--- app.py
+++ app.py
@@ -33,8 +33,12 @@ def author():
 @app.route('/image')
 def image():
     path = url_for('static', filename='oak.jpg')
+    css = url_for('static', filename='lab1.css')
     return '''<!doctype html>
         <html>
+            <head>
+                <link rel="stylesheet" href="''' + css + '''">
+            </head>
            <body>
                <h1>Дуб</h1>
                <img src="''' + path + '''">
