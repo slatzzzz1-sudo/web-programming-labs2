093c73f855dbec43720d165bfceca2aa40e91e80
Самойлов Дмитрий <dima_samoylov_84@mail.ru>
2025-11-18 20:44:56 +0300

    Цикл в шаблоне + исправления

--- app.py
+++ app.py
@@ -414,5 +414,15 @@ def example():
     name = 'Самойлов Дмитрий'
     lab_number = 2
     group = 'ФБИ-32'
-    course = 3
-    return render_template('example.html', name=name, lab_number=lab_number, group=group, course=course)
+    course = '3 курс'
+    fruits = [
+        {'name':'яблоки', 'price':100},
+        {'name':'груши', 'price':120},
+        {'name':'апельсины', 'price':80},
+        {'name':'мандарины', 'price':95},
+        {'name':'манго', 'price':321},
+        ]
+    return render_template('example.html', name=name, 
+                            lab_number=lab_number, group=group, 
+                            course=course, fruits=fruits)
+
