a24fe134410c35f7b33df8e1941279a3ec693fd4
Самойлов Дмитрий <dima_samoylov_84@mail.ru>
2025-11-18 20:17:31 +0300

    Подключение стилей

--- app.py
+++ app.py
@@ -412,4 +412,7 @@ def add_flower(name):
 @app.route('/lab2/example')
 def example():
     name = 'Самойлов Дмитрий'
-    return render_template('example.html', name=name)
+    lab_number = 2
+    group = 'ФБИ-32'
+    course = 3
+    return render_template('example.html', name=name, lab_number=lab_number, group=group, course=course)
