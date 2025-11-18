b00fad66d357cf0a55736946dce9b6e0db92c339
Самойлов Дмитрий <dima_samoylov_84@mail.ru>
2025-11-19 01:00:29 +0300

    Фавиконы и испарваление 1ой лабараторной

--- app.py
+++ app.py
@@ -1,233 +1,226 @@
 from flask import Flask, url_for, request, redirect, Response, abort, render_template
 import datetime
+
 app = Flask(__name__)
 
+# -------------------- LAB1 WEB --------------------
 @app.route("/lab1/web")
 def web():
     return """<!doctype html>
 <html>
 <body>
 <h1>web-cepsep на flask</h1>
-<a href="/author">author</a>
+<a href="/lab1/author">author</a>
 </body>
 </html>""", 200, {
-    "X-Server": "sample",
-    "Content-Type": "text/html; charset=utf-8"
-}
+        "X-Server": "sample",
+        "Content-Type": "text/html; charset=utf-8"
+    }
 
-@app.route('/lab1/image')
+
+# -------------------- LAB1 AUTHOR --------------------
+@app.route('/lab1/author')
 def author():
     name = 'Самойлов Дмитрий Алекссевич'
     group = 'ФБИ-32'
     faculty = 'ФБ'
-    return '''<!doctype html>
-        <html>
-           <body>
-               <p>Студент: ''' + name + '''</p>
-               <p>Группа: ''' + group + '''</p>
-               <p>Факультет: ''' + faculty + '''</p>
-               <a href="/web">web</a>
-           </body>
-        </html>'''
+    return f'''<!doctype html>
+<html>
+   <body>
+       <p>Студент: {name}</p>
+       <p>Группа: {group}</p>
+       <p>Факультет: {faculty}</p>
+       <a href="/lab1/web">web</a>
+   </body>
+</html>'''
 
-@app.route('/lab1/counter')
+
+# -------------------- LAB1 IMAGE --------------------
+@app.route('/lab1/image')
 def image():
     path = url_for('static', filename='oak.jpg')
     css = url_for('static', filename='lab1.css')
 
     html = f'''<!doctype html>
-        <html>
-            <head>
-                <link rel="stylesheet" href="''' + css + '''">
-                <meta charset="utf-8">
-                <link rel="stylesheet" href="{css}">
-                <title>Дуб</title>
-            </head>
-            <body>
-                <h1>Дуб</h1>
-                <img src="{path}">
-            </body>
-        </html>'''
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{css}">
+        <title>Дуб</title>
+    </head>
+    <body>
+        <h1>Дуб</h1>
+        <img src="{path}">
+    </body>
+</html>'''
 
-
-    # создаём объект ответа
     response = Response(html)
-
-    # стандартный заголовок Content-Language
     response.headers['Content-Language'] = 'ru'
-
-    # два произвольных (нестандартных) заголовка
     response.headers['X-Developer'] = 'Samoylov Dima'
     response.headers['X-Lab-Work'] = 'Lab1'
-
     return response
 
+
+# -------------------- COUNTER --------------------
 count = 0
 
-@app.route('/counter')
+@app.route('/lab1/counter')
 def counter():
     global count
     count += 1
     time = datetime.datetime.today()
     url = request.url
     client_ip = request.remote_addr
-    return '''<!doctype html>
-        <html>
-           <body>
-                Сколько раз вы сюда заходили: ''' + str(count) + '''<br>
-               <a href="/lab1/cleencounter">Очистка счётчика</a>
-               <hr>
-               Дата и время: ''' + str(time) + '''<br>
-               Запрошенный адрес: ''' + url + '''<br>
-               Ваш IP-адрес: ''' + client_ip + '''<br> 
-           </body>
-        </html>'''
+    return f'''<!doctype html>
+<html>
+   <body>
+        Сколько раз вы сюда заходили: {count}<br>
+       <a href="/lab1/cleencounter">Очистка счётчика</a>
+       <hr>
+       Дата и время: {time}<br>
+       Запрошенный адрес: {url}<br>
+       Ваш IP-адрес: {client_ip}<br>
+   </body>
+</html>'''
+
 
 @app.route('/lab1/cleencounter')
 def cleencounter():
     global count
     count = 0
-    return '''<!doctype html>
-        <html>
-           <body>
-               Счётчик очищен! <br>
-               Сколько раз вы сюда заходили: ''' + str(count) + '''<br>
-               <a href="/lab1/counter">Вернуться на счётчик</a>
-           </body>
-        </html>'''
+    return f'''<!doctype html>
+<html>
+   <body>
+       Счётчик очищен! <br>
+       Сколько раз вы сюда заходили: {count}<br>
+       <a href="/lab1/counter">Вернуться на счётчик</a>
+   </body>
+</html>'''
 
 
+# -------------------- INFO REDIRECT --------------------
 @app.route('/lab1/info')
 def info():
     return redirect('/lab1/author')
 
+
+# -------------------- CREATED 201 --------------------
 @app.route('/lab1/created')
 def created():
     return '''<!doctype html>
-        <html>
-           <body>
-               <h1>Создано успешно</h1>
-               <div><i>что-то создано...</i></div>
-           </body>
-        </html>''', 201
+<html>
+   <body>
+       <h1>Создано успешно</h1>
+       <div><i>что-то создано...</i></div>
+   </body>
+</html>''', 201
 
+
+# -------------------- 404 CUSTOM --------------------
 @app.route('/404')
 def error404():
     dog = url_for('static', filename='404.jpg')
-    return '''<!doctype html>
-    <html>
-        <head>
-            <meta charset="utf-8">
-            <title>Страница не найдена</title>
-            <style>
-                body {
-                    font-family: Arial, sans-serif;
-                    background: #f9fafc;
-                    color: #333;
-                    text-align: center;
-                    padding: 50px;
-                }
-                h1 {
-                    font-size: 48px;
-                    margin-bottom: 20px;
-                    color: #d9534f;
-                }
-                p {
-                    font-size: 20px;
-                    margin-bottom: 30px;
-                }
-                a {
-                    color: #0275d8;
-                    text-decoration: none;
-                    font-weight: bold;
-                }
-                a:hover {
-                    text-decoration: underline;
-                }
-                img {
-                    width: 250px;
-                    margin-top: 20px;
-                }
-            </style>
-        </head>
-        <body>
-            <h1>404 — Ой! Страница потерялась...</h1>
-            <p>Похоже, такой страницы у нас нет.<br>
-               Но вы всегда можете вернуться <a href="/">на главную</a>.
-            </p>
-            <br>
-            <img src="''' + dog + '''" alt="404 dog">
-        </body>
-    </html>
-    ''', 404
+    return f'''<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Страница не найдена</title>
+        <style>
+            body {{
+                font-family: Arial, sans-serif;
+                background: #f9fafc;
+                color: #333;
+                text-align: center;
+                padding: 50px;
+            }}
+            h1 {{
+                font-size: 48px;
+                margin-bottom: 20px;
+                color: #d9534f;
+            }}
+            p {{
+                font-size: 20px;
+                margin-bottom: 30px;
+            }}
+            a {{
+                color: #0275d8;
+                text-decoration: none;
+                font-weight: bold;
+            }}
+            a:hover {{
+                text-decoration: underline;
+            }}
+            img {{
+                width: 250px;
+                margin-top: 20px;
+            }}
+        </style>
+    </head>
+    <body>
+        <h1>404 — Ой! Страница потерялась...</h1>
+        <p>Похоже, такой страницы у нас нет.<br>
+           Но вы всегда можете вернуться <a href="/">на главную</a>.
+        </p>
+        <br>
+        <img src="{dog}" alt="404 dog">
+    </body>
+</html>
+''', 404
 
 
+# -------------------- INDEX --------------------
 @app.route('/')
 @app.route('/index')
 def index():
     return '''<!doctype html>
-        <html>
-            <head>
-                <title>НГТУ, ФБ, Лабораторные работы</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <header>
-                    НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
-                </header>
-                <ul>
-                    <li><a href="/lab1">Лабораторная работа 1</a></li>
-                    <li><a href="/lab2">Лабораторная работа 2</a></li>
-                </ul>
-                <hr>
-                <footer>
-                    Самойлов Дмитрий Алексеевич, ФБИ-32, 3 курс, 2025
-                </footer>
-            </body>
-                </html>'''
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+        <ul>
+            <li><a href="/lab1">Лабораторная работа 1</a></li>
+            <li><a href="/lab2">Лабораторная работа 2</a></li>
+        </ul>
+        <hr>
+        <footer>
+            Самойлов Дмитрий Алексеевич, ФБИ-32, 3 курс, 2025
+        </footer>
+    </body>
+</html>'''
 
 
+# -------------------- LAB1 MENU --------------------
 @app.route('/lab1')
 def lab1():
     return '''<!doctype html>
-        <html>
-            <head>
-                <title>Лабораторная 1</title>
-            </head>
-            <body>
-                <p>
-                    Flask — фреймворк для создания веб-приложений на языке
-                    программирования Python, использующий набор инструментов
-                    Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
-                    называемых микрофреймворков — минималистичных каркасов
-                    веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
-                </p>
-                <a href="/">Корень сайта</a>
-                                <h2>Список роутов</h2>
-                <ul>
-                    <li><a href="/">Главное меню</a></li>
-                    <li><a href="/index">Главное меню</a></li>
-                    <li><a href="/lab1">Первая лабораторная</a></li>
-                    <li><a href="/lab1/web">Страница с кодом html</a></li>
-                    <li><a href="/lab1/author">Автор</a></li>
-                    <li><a href="/lab1/image">Страница с картинкой</a></li>
-                    <li><a href="/lab1/counter">Счётчик</a></li>
-                    <li><a href="/lab1/cleencounter">Очистка счётчика</a></li>
-                    <li><a href="/lab1/info">Info (redirect)</a></li>
-                    <li><a href="/lab1/created">Создано (201)</a></li>
-                    <li><a href="/400">Ошибка 400</a></li>
-                    <li><a href="/401">Ошибка 401</a></li>
-                    <li><a href="/402">Ошибка 402</a></li>
-                    <li><a href="/403">Ошибка 403</a></li>
-                    <li><a href="/404">Ошибка 404</a></li>
-                    <li><a href="/405">Ошибка 405</a></li>
-                    <li><a href="/418">Ошибка 418</a></li>
-                    <li><a href="/error">Искусственная ошибка (500) (/error)</a></li>
-                </ul>
-            </body>
-        </html>'''
-
-
+<html>
+    <head>
+        <title>Лабораторная 1</title>
+    </head>
+    <body>
+        <p>
+            Flask — фреймворк для создания веб-приложений на языке Python.
+        </p>
+        <a href="/">Корень сайта</a>
+        <h2>Список роутов</h2>
+        <ul>
+            <li><a href="/">Главное меню</a></li>
+            <li><a href="/lab1/web">Страница с HTML</a></li>
+            <li><a href="/lab1/author">Автор</a></li>
+            <li><a href="/lab1/image">Страница с картинкой</a></li>
+            <li><a href="/lab1/counter">Счётчик</a></li>
+            <li><a href="/lab1/cleencounter">Очистка счётчика</a></li>
+            <li><a href="/lab1/info">Info (redirect)</a></li>
+            <li><a href="/lab1/created">Создано (201)</a></li>
+            <li><a href="/404">Ошибка 404</a></li>
+        </ul>
+    </body>
+</html>'''
 
 @app.route('/400')
 def error400():
