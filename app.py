d32502719312ea038ff76c557279e677fb725836
Самойлов Дмитрий <dima_samoylov_84@mail.ru>
2025-11-18 21:11:11 +0300

    Фильтры

--- app.py
+++ app.py
@@ -429,3 +429,9 @@ def example():
 def lab2():
     return render_template('lab2.html')
 
+
+@app.route('/lab2/filters')
+def filters():
+    phrase = '0 <b>сколько</b> <u>нам</u> <i>открытий</i> чудных...'
+    return render_template('filter.html', phrase = phrase)
+
