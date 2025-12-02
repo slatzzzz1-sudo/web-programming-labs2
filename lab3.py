1e353da37221e3199752ea792a76f91877124138
Самойлов Дмитрий <dima_samoylov_84@mail.ru>
2025-12-02 21:15:07 +0700

    Формы

--- lab3.py
+++ lab3.py
@@ -12,7 +12,7 @@ def cookie():
     resp = make_response(redirect('/lab3/'))
     resp.set_cookie('name', 'Alex', max_age=5)
     resp.set_cookie('age', '21')
-    resp.set_cookie('name_color', 'magenta')
+    resp.set_cookie('name_color', 'blue')
     return resp
 
 
@@ -23,3 +23,11 @@ def del_cookie():
     resp.delete_cookie('age')
     resp.delete_cookie('name_color')
     return resp
+
+
+@lab3.route('/lab3/form1')
+def form1():
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex)
