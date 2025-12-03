a63cc63d1600e74a83a55fcda4731782d7891780
Самойлов Дмитрий <dima_samoylov_84@mail.ru>
2025-12-03 14:31:36 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -208,3 +208,42 @@ def login():
 def logout():
     session.pop('login', None)
     return redirect('/lab4/login')
+
+
+@lab4.route('/lab4/fridge', methods=['GET', 'POST'])
+def fridge():
+    message = ''
+    snowflakes = 0
+    temperature = ''
+    if request.method == 'POST':
+        temperature = request.form.get('temperature', '').strip()
+
+        if temperature == '':
+            message = 'Ошибка: не задана температура'
+        else:
+            try:
+                t = int(temperature)
+
+                if t < -12:
+                    message = 'Не удалось установить температуру — слишком низкое значение'
+                elif t > -1:
+                    message = 'Не удалось установить температуру — слишком высокое значение'
+                else:
+                    # диапазоны
+                    if -12 <= t <= -9:
+                        message = f'Установлена температура: {t}°C'
+                        snowflakes = 3
+                    elif -8 <= t <= -5:
+                        message = f'Установлена температура: {t}°C'
+                        snowflakes = 2
+                    elif -4 <= t <= -1:
+                        message = f'Установлена температура: {t}°C'
+                        snowflakes = 1
+            except ValueError:
+                message = 'Ошибка: температура должна быть числом'
+    return render_template(
+        '/lab4/fridge.html',
+        message=message,
+        snowflakes=snowflakes,
+        temperature=temperature
+    )
