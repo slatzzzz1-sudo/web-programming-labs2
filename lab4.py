97632062e2d76dc645cd56458af42acdd6f9f13d
Самойлов Дмитрий <dima_samoylov_84@mail.ru>
2025-12-03 14:05:30 +0700

    вылидация

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 
 lab4 = Blueprint('lab4', __name__)
 
@@ -120,6 +120,8 @@ def tree():
     operation = request.form.get('operation')
     if operation == 'cut':
         tree_count -= 1
+        if tree_count < 0:
+            tree_count = 0
     elif operation == 'plant':
         tree_count += 1
-    return render_template('/lab4/tree.html', tree_count=tree_count)
+    return redirect('/lab4/tree')
