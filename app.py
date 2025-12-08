a5a01ae2fe7b027b969ca9294b87eb7822c95dfd
Самойлов Дмитрий <dima_samoylov_84@mail.ru>
2025-12-08 17:44:46 +0700

    аунтификация

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
+from lab5 import lab5
 
 
 app = Flask(__name__)
@@ -12,6 +13,7 @@ app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
+app.register_blueprint(lab5)
 
 
 app.secret_key = 'секретно-секретный секрет'
@@ -35,6 +37,7 @@ def index():
                     <li><a href="/lab2">Лабораторная работа 2</a></li>
                     <li><a href="/lab3/">Лабораторная работа 3</a></li>
                     <li><a href="/lab4">Лабораторная работа 4</a></li>
+                    <li><a href="/lab5">Лабораторная работа 5</a></li>
                 </ul>
                 <hr>
                 <footer>
