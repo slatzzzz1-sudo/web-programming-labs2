234b9142b2ce846e246d7d08ed497c5a2edf0aae
Самойлов Дмитрий <dima_samoylov_84@mail.ru>
2025-12-08 18:05:30 +0700

    вывод статьей

--- lab5.py
+++ lab5.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, redirect
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -40,13 +40,14 @@ def register():
 
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    cur.execute("SELECT login FROM users WHERE login=%s;", (login, ))
     if cur.fetchone():
         db_close(conn, cur)
         return render_template('lab5/register.html',
                                error='Такой пользователь уже существует')
     password_hash = generate_password_hash(password)
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")
+    cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);",
+                (login, password_hash, ))
     db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
 
@@ -62,7 +63,7 @@ def login():
     conn, cur = db_connect()
     cur = conn.cursor(cursor_factory=RealDictCursor)
 
-    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
     user = cur.fetchone()
 
     if not user:
@@ -77,3 +78,46 @@ def login():
     db_close(conn, cur)
     return render_template('lab5/success_login.html',
                            login=login)
+
+@lab5.route('/lab5/create', methods=['GET', 'POST'])
+def create():
+    login = session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+
+    if request.method == 'GET':
+        return render_template('lab5/create_articles.html')
+
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+
+    conn, cur = db_connect()
+
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
+    login_id = cur.fetchone()["id"]
+
+    cur.execute(
+        "INSERT INTO articles (user_id, title, article_text) VALUES (%s, %s, %s)",
+        (login_id, title, article_text)
+        )
+
+    db_close(conn, cur)
+    return redirect('/lab5')
+
+
+@lab5.route('/lab5/list')
+def list():
+    login = session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+
+    conn, cur = db_connect()
+
+    cur.execute("SELECT id FROM users WHERE login=%s;", (login, ))
+    login_id = cur.fetchone()["id"]
+
+    cur.execute("SELECT * FROM articles WHERE user_id=%s;", (login_id, ))
+    articles = cur.fetchall()
+
+    db_close(conn, cur)
+    return render_template('/lab5/articles.html', articles=articles)
