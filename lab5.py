51d7f09e573db5641c0032722da3ac41e7628ce6
Самойлов Дмитрий <dima_samoylov_84@mail.ru>
2025-12-09 23:11:38 +0700

    fix

--- lab5.py
+++ lab5.py
@@ -11,7 +11,7 @@ lab5 = Blueprint('lab5', __name__)
 
 @lab5.route('/lab5/')
 def lab():
-    return render_template('lab5/errr.html', login=session.get('login'))
+    return render_template('lab5/errr.html')
 
 @lab5.route('/lab5/err')
 def err():
