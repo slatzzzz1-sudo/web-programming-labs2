f62c23ad61961193ca5eb6a88242bc41409cc0a2
Самойлов Дмитрий <dima_samoylov_84@mail.ru>
2025-12-17 01:25:16 +0700

    авторизация

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import os
 from os import path
 from flask_sqlalchemy import SQLAlchemy
 from db import db
+from db.models import users
+from flask_login import LoginManager
 
 
 
@@ -18,6 +20,18 @@ from lab8 import lab8
 
 
 app = Flask(__name__)
+
+
+login_manager = LoginManager()
+login_manager.login_view = 'lab8.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_users(login_id):
+    return users.query.get(int(login_id))
+
+
+
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
--- lab8.py
+++ lab8.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from db import db
+from db.models import users, articles
+from flask_login import login_user, login_required, current_user
 
 
 lab8 = Blueprint('lab8', __name__)
@@ -7,3 +10,46 @@ lab8 = Blueprint('lab8', __name__)
 @lab8.route('/lab8/')
 def lab():
     return render_template('lab8/lab8.html')
+
+
+@lab8.route('/lab8/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    login_exists = users.query.filter_by(login = login_form).first()
+    if login_exists:
+        return render_template('/lab8/register.html', error='Такой пользователь уже существует')
+    
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login = login_form, password = password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/lab8/')
+
+
+@lab8.route('/lab8/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('/lab8/login.html')
+
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    user = users.query_by(login = login_form).first()
+
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember=False)
+            return redirect('/lab8/')
+
+    return render_template('/lab8/login.html', error='Ошибка входа: логин и/или пароль неверны')
+
+
+@lab8.route('/lab8/articles')
+@login_required
+def article_list():
+    return "список статей"
